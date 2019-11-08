08dddb90ab9adf4d21faf5af0b4ff8542dd71bc5
Farid Hamida <faridhamida22@gmail.com>
2019-11-07 22:21:05 -0800

    Updates

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,16 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import {Col, Card, CardText, CardTitle, CardImg, CardBody} from "reactstrap";
 
+const NasaCard2 = styled.div`
+   background: none;
+   color: darkblack;
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+`;
+
+const BackColors = styled.div`
+background: yellow;
+`;
 
 const NasaCard = props => {
 
     return (
-        <div className="nasa-card">
-            <img src= {props.hdurl} alt="space"/>
-           <h2>{props.title}</h2>
-           <p>{props.date}</p> 
-           <p>{props.explanation}</p>
-           
-        </div>
+
+        <NasaCard2>
+            <Col xs="12" s= "6" md="4">
+                
+                <Card>    
+           <CardImg src= {props.hdurl} alt="space"/>
+           <CardBody>
+            <BackColors>
+           <CardTitle>{props.title}</CardTitle>
+            <CardTitle>{props.date}</CardTitle> 
+            <CardText>{props.explanation}</CardText>
+                        </BackColors>
+                    </CardBody>
+                </Card>
+               
+           </Col>
+        </NasaCard2>
     )
 }
 
